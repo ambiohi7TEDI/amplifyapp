e8542f85dc733ddfa253e74413601b2fec57ee1e
ambiohi7TEDI <47255117+ambiohi7TEDI@users.noreply.github.com>
2023-02-14 23:33:32 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          why it aint changing.
+          why it aint changing
         </p>
         <a
           className="App-link"
