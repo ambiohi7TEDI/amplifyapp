1187fa30d6cb8d46dca3782bca6db3782134d9bb
ambiohi7TEDI <47255117+ambiohi7TEDI@users.noreply.github.com>
2023-02-14 23:06:35 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-const AfricasTalking = require('africastalking');
-
-// TODO: Initialize Africa's Talking
-const africastalking = AfricasTalking({
-  apiKey: '8e4e9e6741c556140be2696474b903900bfcb153ad7824a963ce286a1bb6d6ea', 
-  username: 'sandbox'
-});
-
-
-module.exports = async function sendSMS() {
-    
-    // TODO: Send message
-try {
-  const result=await africastalking.SMS.send({
-    to: '+2349162153261', 
-    message: 'Welcome to oil serv',
-    from: 'OILSERVLTD'
-  });
-  console.log(result);
-} catch(ex) {
-  console.error(ex);
-} 
-};
-//is this thing saving
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Welcome to oil serv limited fam. work? 
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+          
+  );
+}
 
 export default App;
