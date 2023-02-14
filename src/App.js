4952eb20b218f4edda9916c3c52aa123e7861e76
ambiohi7TEDI <47255117+ambiohi7TEDI@users.noreply.github.com>
2023-02-14 23:04:24 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,30 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to oil serv limited fam. work? 
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-<script>
-            const AfricasTalking = require('africastalking');
+const AfricasTalking = require('africastalking');
 
 // TODO: Initialize Africa's Talking
 const africastalking = AfricasTalking({
-  apiKey: 'a233a2fd1fcb1157342931bf42be7fc2460c836279fad72c91ffee2b820b4f85', 
+  apiKey: '8e4e9e6741c556140be2696474b903900bfcb153ad7824a963ce286a1bb6d6ea', 
   username: 'sandbox'
 });
 
@@ -34,7 +12,7 @@ module.exports = async function sendSMS() {
     // TODO: Send message
 try {
   const result=await africastalking.SMS.send({
-    to: '+2349045341885', 
+    to: '+2349162153261', 
     message: 'Welcome to oil serv',
     from: 'OILSERVLTD'
   });
@@ -44,8 +22,5 @@ try {
 } 
 };
 //is this thing saving
-</script>
-  );
-}
 
 export default App;
