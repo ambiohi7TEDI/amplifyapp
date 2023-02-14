cb70954d654db32d3a0a24015080d6508f29eca1
ambiohi7TEDI <47255117+ambiohi7TEDI@users.noreply.github.com>
2023-02-14 23:08:33 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to oil serv limited fam. work? 
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -16,7 +19,6 @@ function App() {
         </a>
       </header>
     </div>
-          
   );
 }
 
