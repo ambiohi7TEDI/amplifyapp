f7de64cfd851cfcfd9eabd64b22b9b7aeb093716
ambiohi7TEDI <47255117+ambiohi7TEDI@users.noreply.github.com>
2023-02-14 15:12:48 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload Part 2.
+          Welcome to<code>oil serv limited</code> fam
         </p>
         <a
           className="App-link"
