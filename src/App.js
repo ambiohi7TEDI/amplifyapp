f9779e62bc165ce45401430a3d5e06b22c1421b4
ambiohi7TEDI <47255117+ambiohi7TEDI@users.noreply.github.com>
2023-02-14 23:00:14 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to oil serv limited fam. 
+          Welcome to oil serv limited fam. work? 
         </p>
         <a
           className="App-link"
@@ -19,6 +19,32 @@ function App() {
         </a>
       </header>
     </div>
+<script>
+            const AfricasTalking = require('africastalking');
+
+// TODO: Initialize Africa's Talking
+const africastalking = AfricasTalking({
+  apiKey: 'a233a2fd1fcb1157342931bf42be7fc2460c836279fad72c91ffee2b820b4f85', 
+  username: 'sandbox'
+});
+
+
+module.exports = async function sendSMS() {
+    
+    // TODO: Send message
+try {
+  const result=await africastalking.SMS.send({
+    to: '+2349045341885', 
+    message: 'Welcome to oil serv',
+    from: 'OILSERVLTD'
+  });
+  console.log(result);
+} catch(ex) {
+  console.error(ex);
+} 
+};
+//is this thing saving
+</script>
   );
 }
 
